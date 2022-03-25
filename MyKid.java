3c498810411f09fa591bef79428281eb58276e39
Jayachitra <102034707+AmmuChitra@users.noreply.github.com>
2022-03-26 00:06:45 +0530

    Add files via upload

---
+++ MyKid.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class MyKid {
+	
+	public void buyCar() {
+		System.out.println("Baleno");
+	}
+	public void saleCar() {
+		System.out.println("Skoda");
+	}
+	public static void main(String[] args) {
+		
+
+
+			
+        MyKid Price = new MyKid();
+				Price.buyCar();
+				Price.saleCar();// TODO Auto-generated method stub
+
+	}
+
+	
+
+}
